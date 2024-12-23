c041ee63358ba14383ff7ab113cbeefda45a8c3e
Benedikt Reiser <benedikt.reiser@gmail.com>
2024-12-23 21:01:19 +0100

    refactor: use carbonite logger consistently

--- source/extensions/innoactive.serverextension/innoactive/serverextension/extension.py
+++ source/extensions/innoactive.serverextension/innoactive/serverextension/extension.py
@@ -3,7 +3,6 @@ import omni.ext
 import omni.ui as ui
 import omni.usd
 import omni.kit
-import omni.kit.app
 import omni.kit.commands
 import json
 import carb
@@ -16,8 +15,9 @@ from omni.kit.viewport.utility import get_active_viewport
 
 # Functions and vars are available to other extensions as usual in python: `innoactive.serverextension.some_public_function(x)`
 def set_usd(usd):
-    print(f"[innoactive.serverextension] set_usd '{usd}'")
+    carb.log_info(f"set_usd '{usd}'")
     MyExtension.set_usd(MyExtension, usd)
+
 
 # Any class derived from `omni.ext.IExt` in the top level module (defined in `python.modules` of `extension.toml`) will
 # be instantiated when the extension gets enabled, and `on_startup(ext_id)` will be called.
@@ -72,25 +72,29 @@ class MyExtension(omni.ext.IExt):
             carb.log_error(f"[innoactive.serverextension] Unknown action: {desired_action}")
 
     def set_usd(self, usd_file):
-        print(f"[innoactive.serverextension] internal set_usd '{usd_file}'")
+        carb.log_info(f"internal set_usd '{usd_file}'")
         self.usd_to_load = usd_file
 
-    def _ensure_camera_temp(self, camera_path="/SessionLayer/XRCam", position=(0, 0, 0)):
+    def _ensure_camera_temp(
+        self, camera_path="/SessionLayer/XRCam", position=(0, 0, 0)
+    ):
         """
         Ensures a temporary camera with the specified name exists in the stage.
         If not, it creates one at the given position.
         Sets the camera as the active camera for the viewport.
         """
         if not self.stage:
-            print("[innoactive.serverextension] Stage is not loaded.")
+            carb.log_warning("Stage is not loaded.")
             return
 
         # Check if the camera already exists
         camera_prim = self.stage.GetPrimAtPath(camera_path)
         if camera_prim and camera_prim.IsValid():
-            print(f"[innoactive.serverextension] Camera '{camera_path}' already exists.")
+            carb.log_info(f"Camera '{camera_path}' already exists.")
         else:
-            print(f"[innoactive.serverextension] Camera '{camera_path}' not found. Adding it to the stage (temporary).")
+            carb.log_info(
+                f"Camera '{camera_path}' not found. Adding it to the stage (temporary)."
+            )
             try:
                 # Add the camera in the session layer
                 with Usd.EditContext(self.stage, self.stage.GetSessionLayer()):
@@ -98,11 +102,15 @@ class MyExtension(omni.ext.IExt):
                     camera = UsdGeom.Camera(camera_prim)
 
                     # Set camera's translation
-                    #camera_prim.GetAttribute("xformOp:translate").Set(Gf.Vec3d(*position))
+                    # camera_prim.GetAttribute("xformOp:translate").Set(Gf.Vec3d(*position))
 
-                print(f"[innoactive.serverextension] Temporary Camera '{camera_path}' added successfully at {position}.")
+                carb.log_info(
+                    f"Temporary Camera '{camera_path}' added successfully at {position}."
+                )
             except Exception as e:
-                print(f"[innoactive.serverextension] Failed to add temporary Camera '{camera_path}': {str(e)}")
+                carb.log_info(
+                    f"Failed to add temporary Camera '{camera_path}': {str(e)}"
+                )
 
         # Set the camera as active in the viewport
         # self._set_active_camera_in_viewport(camera_path)
@@ -116,30 +124,33 @@ class MyExtension(omni.ext.IExt):
             if not viewport:
                 raise RuntimeError("No active Viewport")
             viewport.camera_path = camera_path
-            print(f"[innoactive.serverextension] Camera '{camera_path}' set as active in the viewport.")
+            carb.log_info(f"Camera '{camera_path}' set as active in the viewport.")
         except Exception as e:
-            print(f"[innoactive.serverextension] Failed to set active camera in the viewport '{camera_path}': {str(e)}")
+            carb.log_info(
+                f"Failed to set active camera in the viewport '{camera_path}': {str(e)}"
+            )
 
     def _on_stage_event(self, event):
         if event.type == int(omni.usd.StageEventType.OPENED):
-            print("[innoactive.serverextension] Stage has fully loaded!")
+            carb.log_info("Stage has fully loaded!")
             stage_path = self.usd_context.get_stage_url()
-            print(f"[innoactive.serverextension] Stage {stage_path}")
+            carb.log_info(f"Stage {stage_path}")
 
             # Get the stage and store it at the class level
             self.stage = self.usd_context.get_stage()
             if not self.stage:
-                print("[innoactive.serverextension] Unable to retrieve stage.")
+                carb.log_info("Unable to retrieve stage.")
                 return
 
             if stage_path.startswith("anon:"):
                 delay = 1
-                print(f"[innoactive.serverextension] empty_stage loaded. Now loading USD file with delay of {delay} seconds")
+                carb.log_info(
+                    f"empty_stage loaded. Now loading USD file with delay of {delay} seconds"
+                )
                 asyncio.ensure_future(self._delayed_load_usd(delay))
             else:
-                print(f"[innoactive.serverextension] USD loaded: {stage_path}")
+                carb.log_info(f"USD loaded: {stage_path}")
                 self.load_layout()
-
 
     def load_usd(self, usd_file: str, log_errors=True):
         """
@@ -150,11 +161,11 @@ class MyExtension(omni.ext.IExt):
         """
         if not isinstance(usd_file, str):
             if log_errors:
-                carb.log_error(f"[innoactive.serverextension] Invalid USD path: {usd_file}. Must be a string.")
+                carb.log_error(f"Invalid USD path: {usd_file}. Must be a string.")
             return
 
         try:
-            carb.log_info(f"[innoactive.serverextension] Loading USD file: {usd_file}")
+            carb.log_info(f"Loading USD file: {usd_file}")
             omni.usd.get_context().open_stage(usd_file)
 
             # If AR, then ensure the XRCam camera exists
@@ -164,9 +175,9 @@ class MyExtension(omni.ext.IExt):
 
         except Exception as e:
             if log_errors:
-                carb.log_error(f"[innoactive.serverextension] Failed to open USD file {usd_file}: {str(e)}")
+                carb.log_error(f"Failed to open USD file {usd_file}: {str(e)}")
 
-    async def _delayed_load_usd(self, delay = 10):
+    async def _delayed_load_usd(self, delay=10):
 
         await asyncio.sleep(delay)
         self.load_usd(usd_file=self.usd_to_load)
@@ -177,63 +188,82 @@ class MyExtension(omni.ext.IExt):
 
         if not os.path.exists(workspace_file):
             if log_errors:
-                carb.log_error(f"[innoactive.serverextension] Layout file does not exist: {workspace_file}")
+                carb.log_error(f"Layout file does not exist: {workspace_file}")
             return
 
         try:
             result, _, content = omni.client.read_file(workspace_file)
             if result != omni.client.Result.OK:
                 if log_errors:
-                    carb.log_error(f"[innoactive.serverextension] Can't read the workspace file {workspace_file}, error code: {result}")
+                    carb.log_error(
+                        f"Can't read the workspace file {workspace_file}, error code: {result}"
+                    )
                 return
 
             try:
                 data = json.loads(memoryview(content).tobytes().decode("utf-8"))
             except Exception as e:
                 if log_errors:
-                    carb.log_error(f"[innoactive.serverextension] Failed to parse JSON from {workspace_file}: {str(e)}")
+                    carb.log_error(
+                        f"Failed to parse JSON from {workspace_file}: {str(e)}"
+                    )
                 return
 
             ui.Workspace.restore_workspace(data, False)
-            carb.log_info(f"[innoactive.serverextension] The workspace is loaded from {workspace_file}")
+            carb.log_info(f"The workspace is loaded from {workspace_file}")
         except Exception as e:
             if log_errors:
-                carb.log_error(f"[innoactive.serverextension] Unexpected error while loading layout: {str(e)}")
+                carb.log_error(f"Unexpected error while loading layout: {str(e)}")
 
     def _apply_ar_settings_after_load(self):
-        print("[innoactive.serverextension] applying AR settings after load")
+        carb.log_info("applying AR settings after load")
         # Innoactive AR Settings
         self.settings.set("/xrstage/profile/ar/anchorMode", "scene origin")
         self.settings.set("/xrstage/profile/ar/enableCameraOutput", True)
         self.settings.set("/xrstage/profile/ar/cameraOutputPath", "/SessionLayer/XRCam")
 
-
     def on_startup(self, ext_id):
-        print("[innoactive.serverextension] Extension startup")
+        carb.log_info("Extension startup")
 
         # Access parameters
-        self.interface_mode = self.settings.get_as_string("/innoactive/serverextension/interfaceMode") or "screen"
-        self.usd_to_load = self.settings.get_as_string("/innoactive/serverextension/usdPath") or self.default_usd
-        #print("ANCHOR " + self.settings.get("/xrstage/profile/ar/anchorMode"))
+        self.interface_mode = (
+            self.settings.get_as_string("/innoactive/serverextension/interfaceMode")
+            or "screen"
+        )
+        self.usd_to_load = (
+            self.settings.get_as_string("/innoactive/serverextension/usdPath")
+            or self.default_usd
+        )
+        # carb.log_info("ANCHOR " + self.settings.get("/xrstage/profile/ar/anchorMode"))
 
         if self.interface_mode == "vr":
             # Set the resolution multiplier for VR rendering
             self.settings.set("/persistent/xr/profile/vr/system/display", "SteamVR")
-            self.settings.set("/persistent/xr/profile/vr/render/resolutionMultiplier", 2.0)
-            self.settings.set("/persistent/xr/profile/vr/foveation/mode", "warped") #none / warped / inset
-            self.settings.set("/persistent/xr/profile/vr/foveation/warped/resolutionMultiplier", 0.5)
+            self.settings.set(
+                "/persistent/xr/profile/vr/render/resolutionMultiplier", 2.0
+            )
+            self.settings.set(
+                "/persistent/xr/profile/vr/foveation/mode", "warped"
+            )  # none / warped / inset
+            self.settings.set(
+                "/persistent/xr/profile/vr/foveation/warped/resolutionMultiplier", 0.5
+            )
             settings.set("/persistent/xr/profile/vr/foveation/warped/insetSize", 0.4)
         elif self.interface_mode == "ar":
             self.settings.set("/xr/cloudxr/version", 4.1)
             self.settings.set("/xr/depth/aov", "GBufferDepth")
             self.settings.set("/xr/simulatedxr/enabled", True)
             self.settings.set("/persistent/renderer/raytracingOmm/enabled", True)
-            self.settings.set("/rtx-transient/resourcemanager/enableTextureStreaming", False)
+            self.settings.set(
+                "/rtx-transient/resourcemanager/enableTextureStreaming", False
+            )
             self.settings.set("/xr/ui/enabled", False)
             self.settings.set("/defaults/xr/profile/ar/renderQuality", "off")
             self.settings.set("/defaults/xr/profile/ar/system/display", "CloudXR41")
             self.settings.set("/persistent/xr/profile/ar/render/nearPlane", 0.15)
-            self.settings.set("/persistent/rtx/sceneDb/allowDuplicateAhsInvocation", False)
+            self.settings.set(
+                "/persistent/rtx/sceneDb/allowDuplicateAhsInvocation", False
+            )
 
         # Get the USD context
         self.usd_context = omni.usd.get_context()
@@ -255,20 +285,20 @@ class MyExtension(omni.ext.IExt):
 
                 def on_reset_stage():
                     self.load_usd(usd_file=self.empty_stage)
-                    print("on_reset_stage()")
+                    carb.log_info("on_reset_stage()")
 
                 def on_load_layout():
                     self.load_layout()
-                    print("on_load_layout()")
+                    carb.log_info("on_load_layout()")
 
                 with ui.VStack():
-                    #ui.Button("ConfigVR", clicked_fn=config_vr)
+                    # ui.Button("ConfigVR", clicked_fn=config_vr)
                     ui.Button("Load Layout", clicked_fn=on_load_layout)
                     # ui.Button("Load USD", clicked_fn=on_load_usd)
                     # ui.Button("Reset", clicked_fn=on_reset_stage)
 
     def on_shutdown(self):
-        print("[innoactive.serverextension] Extension shutdown")
+        carb.log_info("Extension shutdown")
+
         if self._subscriptions:
             self._subscriptions.clear()
-
